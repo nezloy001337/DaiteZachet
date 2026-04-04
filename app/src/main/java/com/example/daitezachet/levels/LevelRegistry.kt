dc21e3dde0290050eb70b3dd3c74a4db1a6d30c0
Andrian Yukhnevich <andy.yukh@gmail.com>
2026-04-05 00:19:45 +0300

    fixes

--- app/src/main/java/com/example/daitezachet/levels/LevelRegistry.kt
+++ app/src/main/java/com/example/daitezachet/levels/LevelRegistry.kt
@@ -2,22 +2,10 @@ package com.example.daitezachet.levels
 
 object LevelRegistry {
     private val levels: List<Level> = listOf(
-        Level01(), // 1
-        Level02(), // 2
+        Level02(), // 1
         Level03(), // 3
-        Level01(), // 4
-        Level02(), // 5
-        Level03(), // 6
-        Level01(), // 7
-        Level02(), // 8
-        Level03(), // 9
-        Level01(), // 10
-        Level02(), // 11
-        Level03(), // 12
-        Level01(), // 13
-        Level02(), // 14
-        Level03()  // 15
     )
+
 
     val count: Int get() = levels.size
 
